a43b3141cd6bed053879012f47b24c2e3037b76f
Damian Trafiałek <damian.trafialek@gmail.com>
2023-10-06 10:14:46 +0200

    add: function desc

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,9 @@
-def parse_number_to_password(number):
+def parse_number_to_password(number) -> str:
+    """
+    Parse a number to a password.
+    @param number: The number to parse.
+    @return: The parsed number.
+    """
     number = str(number)
     if len(number) < 4:
         number = "0" * (4 - len(number)) + number
