8f39360514d9b08d231804a6e55f8d50390beb98
Damian Trafiałek <damian.trafialek@gmail.com>
2023-11-09 09:17:48 +0100

    validated against new ui

--- main.py
+++ main.py
@@ -4,35 +4,33 @@ from itslearning_auto_logger.logger import queue_for_presence
 from selenium_driver.driver import drivers
 from utils.toml_helper import config
 
+
+def create_in_threads():
+    for i in range(0, config["driver"]["count"]):
+        driver = drivers.get_driver(f"thread_driver{i}")
+        Thread(
+            target=queue_for_presence,
+            args=(
+                driver,
+                10 * 1000 // 15 * i,
+                10 * 1000 // 15 * (i + 1),
+                config["driver"]["repeat"],
+            ),
+        ).start()
+
+
 def main():
     """
     This is the main function of the program. It loads the drivers, wait for registration to open and starts the threads.
     It has to phases. First phase is to wait for registration to open. Second phase is to crack the registration code.
     """
-    print('Auto logger engaging...')
+    print("Auto logger engaging...")
 
-    driver_main = drivers.get_driver('main')
+    driver_main = drivers.get_driver("main")
     queue_for_presence(driver_main, inform_about_registration=True)
     driver_main.quit()
-    
-    for i in range(0, config['driver']['count']):
-        driver = drivers.get_driver(f"driver{i}") 
-        Thread(
-            target=queue_for_presence, 
-            args=( 
-                driver,
-                10*1000//15*i, 
-                10*1000//15*(i+1), 
-                5
-            )
-        ).start()
+    create_in_threads()
 
-        # Allows time for previous thread to load and execute necessary setup
-        time.sleep(1)
- 
 
-    
-    
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
